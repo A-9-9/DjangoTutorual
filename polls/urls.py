e339df5a40985fbefe0a399b457cda4754079e8a
Ken <10846013@ntub.edu.tw>
2022-01-12 16:16:34 +0800

    Part3: Template

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
 # put the route related with views
+# colon separates the converter and the pattern name -> <int:question_id>, <str:text>
+app_name = 'polls'
+# when html invoke the 'url', can differentiate the name with other apps like app_name: name
 urlpatterns = [
+    # /polls/
     path('', views.index, name='index'),
+    # /polls/2/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # /polls/2/results
+    path('<int:question_id>/results', views.result, name='result'),
+    # /polls/2/vote
+    path('<int:question_id>/vote', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,35 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.template import loader
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, world. You are in the polls index.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # return HttpResponse(template.render(context, request))
+
+    # render(request, templates_name, context=None, content_type=None, status=None, using=None)
+    return render(request, 'polls/index.html', context)
+
+def detail(requset, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist.')
+
+    # get_object_or_404(model, **kwargs)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(requset, 'polls/detail.html', {'question': question})
+
+def result(requset, question_id):
+    response = 'You are looking at the result of the question %s.'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('You are voting on the question %s.' % question_id)
+
+
